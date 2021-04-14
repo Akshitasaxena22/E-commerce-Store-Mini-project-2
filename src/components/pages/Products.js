d12fbf9898b1df413650b9473eb47d9c56fa3a18
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2021-04-14 16:15:41 +0530

    changes made

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import '../../App.css';
-
+import {Link} from 'react-router-dom';
 
 const arr=[1,2,3,4,5,6];
 
 const cardItem=arr.map((item,index)=>{
   return(
     <div className="col-12 col-md-4 my-5">
-      <div class="card" key={index} style={{width:"18rem"}}>
-        <img src="..." class="card-img-top" alt="..."/>
+      <div class="card w-100 border border-secondary" style={{backgroundColor: "#e1f5fe"}} key={index}>
+        <img src="https://www.howitworksdaily.com/wp-content/uploads/2015/06/daytime-flow-h2o-1295138-1024x576.jpg" class="card-img-top p-1" style={{height: "160px"}} alt="..."/>
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <h5 class="card-title" style={{textDecoration: "underline", color: "#616161"}}>Card title</h5>
+          <p class="card-text text-justify" style={{color: "grey"}}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn text-uppercase" style={{backgroundColor: "#81d4fa", color: "white"}}>Go somewhere</a>
         </div>
       </div>
     </div>
@@ -22,7 +22,7 @@ const cardItem=arr.map((item,index)=>{
 export default function Products() {
   return(
     <>
-     <h1 className='products'>PRODUCTS</h1>;
+     <h1 className='products'>PRODUCTS</h1>
      <div className="container">
        <div className="row">
            {cardItem}
